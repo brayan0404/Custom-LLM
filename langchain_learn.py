3f4ca6ec00ed2ae733aca1a9a9cc3c8a37df90fc
brayan0404 <85806045+brayan0404@users.noreply.github.com>
2023-06-17 18:59:48 -0500

    Update langchain_learn.py

--- langchain_learn.py
+++ langchain_learn.py
@@ -8,10 +8,12 @@ st.title("Custom LLM")
 st.text("Observa como afectan los parametros que configuras en el llm")
 
 openai_api_key = st.sidebar.text_input("Coloca tu Api de OpenAI")
+if openai_api_key == False:
+    st.sidebar.warning("Coloca tu clave de OpenAI")
+
 model_name = st.sidebar.selectbox("Large Language Model", ["text-davinci-003", "gpt-3.5-turbo"])
 temperature = st.sidebar.slider("Elige la temperature", 0.0,1.0)
 instructions = st.sidebar.text_area("Comportamiento")
-
 
 def response(user_text):
     llm = OpenAI(
